e9e4a2f67ad59bd80746d57669645ba79709eb65
sumith-098 <kumarsumith708@gmail.com>
2026-08-16 08:52:24 +0530

    correct url added

--- payment-service/src/main/java/com/cinebook/payment/controller/PaymentController.java
+++ payment-service/src/main/java/com/cinebook/payment/controller/PaymentController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/payments")
 public class PaymentController {
-
+ 
     private final PaymentService paymentService;
 
     public PaymentController(PaymentService paymentService) {
